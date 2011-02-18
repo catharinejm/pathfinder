f6d892bec269e026f809c56df853bcb44bee27a2
Jon Distad <jon.distad@gmail.com>
2011-02-17 23:15:43 -0500

    corner gettin'

--- thing.rb
+++ thing.rb
@@ -17,33 +17,93 @@ class Thing
   def left() x - width/2.0 end
   def right() x + width/2.0 end
 
-  def left_intersection(stx, sty, edx, edy)
-    px_nmr =
-      Matrix[
-        [Matrix[[stx,sty],[edx,edy]].det, Matrix[[stx,1],[edx,1]].det],
-        [Matrix[[left,top],[left,bottom]].det, Matrix[[left,1],[left,1]].det]
-      ].det
+  def side_intx(stx, sty, edx, edy, side)
     py_nmr =
       Matrix[
         [Matrix[[stx,sty],[edx,edy]].det, Matrix[[sty,1],[edy,1]].det],
-        [Matrix[[left,top],[left,bottom]].det, Matrix[[top,1],[bottom,1]].det]
+        [Matrix[[side,top],[side,bottom]].det, Matrix[[top,1],[bottom,1]].det]
       ].det
     denom =
       Matrix[
         [Matrix[[stx,1],[edx,1]].det, Matrix[[sty,1],[edy,1]].det],
-        [Matrix[[left,1],[left,1]].det, Matrix[[top,1],[bottom,1]].det]
+        [Matrix[[side,1],[side,1]].det, Matrix[[top,1],[bottom,1]].det]
       ].det
 
-    [px_nmr/denom, py_nmr/denom]
+    py = py_nmr/denom
+    py >= top && py <= bottom ? [side, py] : nil
+  end
+
+  def left_intx(stx, sty, edx, edy)
+    side_intx(stx, sty, edx, edy, left)
+  end
+
+  def right_intx(stx, sty, edx, edy)
+    side_intx(stx, sty, edx, edy, right)
+  end
+
+  def tb_intx(stx, sty, edx, edy, tb)
+    px_nmr =
+      Matrix[
+        [Matrix[[stx,sty],[edx,edy]].det, Matrix[[stx,1],[edx,1]].det],
+        [Matrix[[left,tb],[right,tb]].det, Matrix[[left,1],[right,1]].det]
+      ].det
+    denom =
+      Matrix[
+        [Matrix[[stx,1],[edx,1]].det, Matrix[[sty,1],[edy,1]].det],
+        [Matrix[[left,1],[right,1]].det, Matrix[[tb,1],[tb,1]].det]
+      ].det
+
+    px = px_nmr/denom
+    px >= left && px <= right ? [px, tb] : nil
+  end
+
+  def top_intx(stx, sty, edx, edy)
+    tb_intx(stx, sty, edx, edy, top)
+  end
+
+  def bottom_intx(stx, sty, edx, edy)
+    tb_intx(stx, sty, edx, edy, bottom)
   end
 
   def on_path?(stx, sty, edx, edy)
-    false
+    left_intx(stx, sty, edx, edy) || 
+    right_intx(stx, sty, edx, edy) || 
+    top_intx(stx, sty, edx, edy) || 
+    bottom_intx(stx, sty, edx, edy)
   end
 
   def nearest_corner(stx, sty, edx, edy)
-    if stx <= left
-      
+    ti = top_intx(stx, sty, edx, edy)
+    bi = bottom_intx(stx, sty, edx, edy)
+    
+    if stx < edx
+      if sty < edy
+        if ti
+          [top, right]
+        else
+          [bottom, left]
+        end
+      else
+        if bi
+          [bottom, right]
+        else
+          [top, left]
+        end
+      end
+    else
+      if sty < edy
+        if ti
+          [top, left]
+        else
+          [bottom, right]
+        end
+      else
+        if bi
+          [bottom, left]
+        else
+          [top, right]
+        end
+      end
     end
   end
 
